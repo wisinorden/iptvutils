4e486d555853531eb42fb6147110b8416fcb878a
Håkan Gudmundsson <hakangud@gmail.com>
2018-07-25 14:25:07 +0200

    Add player class for MPEG-TS files

--- Player/tsfilenetworkplayer.cpp
+++ Player/tsfilenetworkplayer.cpp
@@ -1,6 +1,94 @@
-#include "tsnetworkplayer.h"
+#include "tsfilenetworkplayer.h"
 
-TsNetworkPlayer::TsNetworkPlayer()
-{
+void TsFileNetworkPlayer::start() {
+    finalStatus.setAnalysisMode(config.getWorkerMode());
+    qInfo("Starting TsFileNetworkPlayer in thread: %p", QThread::currentThread());
+    //producer.addNext(&analyzerMiddleware)->addNext(&consumer);
+    producer.addNext(&consumer);
+    producer.init(&producerThread);
+    connect(&consumer, &TsNetworkConsumer::finished, &producer, &TsBufferedProducer::stop);
+    connect(&producerThread, &QThread::finished, this, &TsFileNetworkPlayer::moduleFinished);
+    connect(analyzerMiddleware.thread(), &QThread::finished, this, &TsFileNetworkPlayer::moduleFinished);
+    connect(loopMiddleware.thread(), &QThread::finished, this, &TsFileNetworkPlayer::moduleFinished);
+    connect(&consumerThread, &QThread::finished, this, &TsFileNetworkPlayer::moduleFinished);
+    connect(&producer, &TsBufferedProducer::status, this, &TsFileNetworkPlayer::gotProducerStatus);
+    //connect(&analyzerMiddleware, &AnalyzerTsMiddleware::status, this, &TsFileNetworkPlayer::gotAnalyzerStatus);
+    connect(&consumer, &TsNetworkConsumer::status, this, &TsFileNetworkPlayer::gotConsumerStatus);
+    connect(&analyzerMiddleware, &AnalyzerTsMiddleware::workerStatus, this, &TsFileNetworkPlayer::workerStatus);
+    producerThread.start();
+    //analyzerMiddleware.start();
+    consumer.start(&consumerThread);
+    emit started();
+}
 
+void TsFileNetworkPlayer::gotProducerStatus(Status pStatus) {
+    if (pStatus.getType() == Status::STATUS_ERROR) {
+        finalStatus.setError(pStatus.getError());
+        stop();
+        emit status(finalStatus);
+        return;
+    }
+}
+
+void TsFileNetworkPlayer::gotAnalyzerStatus(AnalyzerStatus aStatus) {
+    if (aStatus.getType() == Status::STATUS_ERROR) {
+        finalStatus.setError(aStatus.getError());
+        stop();
+        emit status(finalStatus);
+        return;
+    }
+    else if (config.getWorkerMode() == WorkerConfiguration::NORMAL_MODE) {
+        this->finalStatus.setAnalyzerInfo(aStatus);
+        emit status(finalStatus);
+    }
+    else if (config.getWorkerMode() == WorkerConfiguration::ANALYSIS_MODE_OFFLINE) {
+        this->finalStatus.setBase(aStatus);
+        this->finalStatus.setAnalyzerInfo(aStatus);
+        emit status(finalStatus);
+    }
+}
+
+void TsFileNetworkPlayer::gotConsumerStatus(Status cStatus) {
+    if (cStatus.getType() == Status::STATUS_ERROR) {
+        finalStatus.setError(cStatus.getError());
+        stop();
+        emit status(finalStatus);
+        return;
+    }
+    if (config.getWorkerMode() == WorkerConfiguration::NORMAL_MODE) {
+        finalStatus.setBase(cStatus);
+        emit status(finalStatus);
+    }
+}
+
+void TsFileNetworkPlayer::gotProducerWorkerStatus(WorkerStatus worker) {
+    emit workerStatus(worker);
+}
+
+void TsFileNetworkPlayer::moduleFinished() {
+    if (producerThread.isRunning()) return;
+    if (analyzerMiddleware.thread()->isRunning()) return;
+    if (loopMiddleware.thread()->isRunning()) return;
+    if (consumerThread.isRunning()) return;
+    qInfo();
+    emit finished();
+    //if (analyzerMiddleware.thread()->isRunning()) return;
+}
+
+void TsFileNetworkPlayer::stop() {
+    consumer.stop();
+    analyzerMiddleware.stop();
+    //loopMiddleware.stop();
+    producer.stop();
+}
+
+void TsFileNetworkPlayer::stopAndWait() {
+    qInfo("ts stop and wait");
+    stop();
+    qInfo("tsfilenetworkplayer stopandwait stop() done");
+    producerThread.wait();
+    qInfo("tsfilenetworkplayer producerthread.wait() done");
+    analyzerMiddleware.thread()->wait();
+    //loopMiddleware.thread()->wait();
+    consumerThread.wait();
 }
--- Player/tsfilenetworkplayer.h
+++ Player/tsfilenetworkplayer.h
@@ -1,11 +1,41 @@
-#ifndef TSNETWORKPLAYER_H
-#define TSNETWORKPLAYER_H
+#ifndef TSFILENETWORKPLAYER_H
+#define TSFILENETWORKPLAYER_H
 
+#include "player.h"
+#include "../Configuration/workerconfiguration.h"
+#include "../PacketProducer/tsbufferedproducer.h"
+#include "../PacketConsumer/tsnetworkconsumer.h"
+#include "../Middleware/analyzertsmiddleware.h"
+#include "../Middleware/looppcapmiddleware.h"
+#include "Status/analyzerstatus.h"
+#include "Status/finalstatus.h"
 
-class TsNetworkPlayer
+class TsFileNetworkPlayer : public Player
 {
+
+private:
+    TsBufferedProducer producer;
+    TsNetworkConsumer consumer;
+    AnalyzerTsMiddleware analyzerMiddleware;
+    LoopPcapMiddleware loopMiddleware;
+
 public:
-    TsNetworkPlayer();
+    explicit TsFileNetworkPlayer(WorkerConfiguration config, QObject *parent = 0) :
+        Player(config, parent),
+        producer(config),
+        consumer(config),
+        analyzerMiddleware(config),
+        loopMiddleware(config) {}
+    void start();
+    void stop();
+    void stopAndWait();
+
+private slots:
+    void gotProducerStatus(Status pStatus);
+    void gotAnalyzerStatus(AnalyzerStatus aStatus);
+    void gotConsumerStatus(Status cStatus);
+    void gotProducerWorkerStatus(WorkerStatus worker);
+    void moduleFinished();
 };
 
-#endif // TSNETWORKPLAYER_H
+#endif // TSFILENETWORKPLAYER_H
