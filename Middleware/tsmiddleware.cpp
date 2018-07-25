1efecded6cf396f45bda7cb7d3375dc11dd9c91d
Håkan Gudmundsson <hakangud@gmail.com>
2018-07-25 14:50:32 +0200

    Make ts middleware ready for implementation

--- Middleware/tsmiddleware.cpp
+++ Middleware/tsmiddleware.cpp
@@ -1,6 +1,1 @@
 #include "tsmiddleware.h"
-
-TsMiddleware::TsMiddleware()
-{
-
-}
--- Middleware/tsmiddleware.h
+++ Middleware/tsmiddleware.h
@@ -1,11 +1,36 @@
 #ifndef TSMIDDLEWARE_H
 #define TSMIDDLEWARE_H
 
+#include <QThread>
+#include "PacketProducer/productprovider.h"
+#include "Configuration/workerconfiguration.h"
+#include "Status/workerstatus.h"
 
-class TsMiddleware
+#define MIDDLEWARE_BUFFER_SIZE 16
+
+class TsMiddleware : public QObject, public ProductProvider
 {
+    Q_OBJECT
+protected:
+    WorkerConfiguration config;
+    bool stopping = false;
+    QThread runnerThread;
+
 public:
-    TsMiddleware();
+    TsMiddleware(WorkerConfiguration config) :
+        QObject(), config(config) {}
+
+signals:
+    void started();
+    void finished();
+    void workerStatus(WorkerStatus status);
+
+protected slots:
+    virtual void run() = 0;
+
+public slots:
+    virtual void start() = 0;
+    virtual void stop() { stopping = true; }
 };
 
 #endif // TSMIDDLEWARE_H
