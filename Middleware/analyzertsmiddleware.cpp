a1b0a74b9f51dcb2c33f9c8b5e11e4086ed26fcb
Håkan Gudmundsson <hakangud@gmail.com>
2018-07-25 14:53:35 +0200

    Make analyzer ts middleware ready for implementation

--- Middleware/analyzertsmiddleware.cpp
+++ Middleware/analyzertsmiddleware.cpp
@@ -1,6 +1,40 @@
 #include "analyzertsmiddleware.h"
 
-AnalyzerTsMiddleware::AnalyzerTsMiddleware()
-{
+unsigned const MP2T_SIZE = 188;
+unsigned const MP2T_PCR_CLOCK = 27000000;
 
+void AnalyzerTsMiddleware::init() {
+    qInfo("AnalyzerTsMiddleware init");
+    this->moveToThread(&runnerThread);
+    connect(&runnerThread, &QThread::started, this,
+            &AnalyzerTsMiddleware::run);
+    connect(this, &AnalyzerTsMiddleware::finished,
+            &runnerThread, &QThread::quit);
+}
+
+void AnalyzerTsMiddleware::start() {
+    qInfo("AnalyzerTsMiddleware starting");
+    runnerThread.start();
+}
+
+void AnalyzerTsMiddleware::stop() {
+    stopping = true;
+    buffer.stop();
+}
+
+void AnalyzerTsMiddleware::run() {
+    qInfo("starting AnalyzerTsMiddleware");
+    emit started();
+    bufferProducts();
+    qInfo("AnalyzerTsMiddleware finished");
+    emit finished();
+    this->thread()->quit();
+}
+
+void AnalyzerTsMiddleware::bufferProducts() {
+
+}
+
+Product AnalyzerTsMiddleware::getProduct() {
+    return buffer.pop();
 }
--- Middleware/analyzertsmiddleware.h
+++ Middleware/analyzertsmiddleware.h
@@ -1,11 +1,49 @@
 #ifndef ANALYZERTSMIDDLEWARE_H
 #define ANALYZERTSMIDDLEWARE_H
 
+#include "tsmiddleware.h"
+#include "concurrentqueue.h"
+#include "Status/analyzerstatus.h"
+#include "pidinfo.h"
+#include "Analyzer/tsanalyzer.h"
+#include "Analyzer/tserrors.h"
+#include "Status/streaminfo.h"
+#include <QMap>
+#include <QHash>
 
-class AnalyzerTsMiddleware
+class AnalyzerTsMiddleware : public TsMiddleware
 {
+private:
+    ConcurrentQueue<Product> buffer;
+    TsParser tsParser;
+    TsAnalyzer tsAnalyzer;
+    TsErrors tsErrors;
+    QMap<int, PIDInfo> pidMap;
+    qint64 packetNumber;
+    qint64 duration;
+    void bufferProducts();
+    QHash<quint64, StreamInfo> streams;
 public:
-    AnalyzerTsMiddleware();
+    AnalyzerTsMiddleware(WorkerConfiguration config) :
+        TsMiddleware(config),
+        buffer(MIDDLEWARE_BUFFER_SIZE),
+        tsParser(),
+        tsAnalyzer(tsParser, duration, packetNumber)
+    {
+        init();
+    }
+    void init();
+    Product getProduct();
+
+signals:
+    void status(AnalyzerStatus status);
+
+protected slots:
+    void run();
+
+public slots:
+    void start();
+    void stop();
 };
 
 #endif // ANALYZERTSMIDDLEWARE_H
